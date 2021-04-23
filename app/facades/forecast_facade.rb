78b06991e3c1f9ec778ccc3038c762b2b6893e48
abreaux26 <angelbreaux26@gmail.com>
2021-04-23 18:05:12 -0500

    Create open weather service

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,15 +1,11 @@
 class ForecastFacade
   def self.get_forecast(params)
-    # conn = Faraday.new(url: 'http://www.mapquestapi.com')
-    # response = conn.get("geocoding/v1/address?key=#{ENV['mapquest_key']}&location=#{params[:location]}")
-    # data = JSON.parse(response.body, symbolize_names: true)
-    # coordinates = Coordinate.new(data[:results][0][:locations].first[:latLng])
-
     coordinates = MapquestService.get_coordinates(params[:location])
-    
-    conn_2 = Faraday.new(url: 'https://api.openweathermap.org')
-    response_2 = conn_2.get("/data/2.5/onecall?lat=#{coordinates.lat}&lon=#{coordinates.lng}&appid=#{ENV['open_weather_map_key']}")
-    data_2 = JSON.parse(response_2.body, symbolize_names: true)
-    CurrentWeather.new(data_2[:current])
+
+    # conn_2 = Faraday.new(url: 'https://api.openweathermap.org')
+    # response_2 = conn_2.get("/data/2.5/onecall?lat=#{coordinates.lat}&lon=#{coordinates.lng}&appid=#{ENV['open_weather_map_key']}")
+    # data_2 = JSON.parse(response_2.body, symbolize_names: true)
+    # CurrentWeather.new(data_2[:current])
+    OpenWeatherService.current_weather(coordinates)
   end
 end
---
+++ app/services/open_weather_service.rb
@@ -0,0 +1,15 @@
+class OpenWeatherService
+  def self.connection
+    Faraday.new(url: 'https://api.openweathermap.org')
+  end
+
+  def self.get_data(url)
+    response = connection.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.current_weather(coordinates)
+    data = get_data("/data/2.5/onecall?lat=#{coordinates.lat}&lon=#{coordinates.lng}&appid=#{ENV['open_weather_map_key']}")
+    CurrentWeather.new(data[:current])
+  end
+end
