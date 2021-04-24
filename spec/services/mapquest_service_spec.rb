eb07cd0cbecaf78ce0e15a53e616b51f622f10a9
abreaux26 <angelbreaux26@gmail.com>
2021-04-24 09:40:20 -0500

    Create spec for mapquest service

---
+++ spec/services/mapquest_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+RSpec.describe 'Mapquest Service' do
+  describe 'happy path' do
+    it '::get_coordinates(location)' do
+      VCR.use_cassette('mapquest_engine/coordinates') do
+        coords = MapquestService.get_coordinates('denver,co')
+
+        expect(coords).to be_a(Hash)
+        expect(coords).to have_key(:lat)
+        expect(coords).to have_key(:lng)
+
+        expect(coords[:lat]).to be_a(Float)
+        expect(coords[:lng]).to be_a(Float)
+      end
+    end
+  end
+
+  describe 'sad path' do
+  end
+end
