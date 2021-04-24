8764743fa1582bb7addc13c73784bb0b461330cb
abreaux26 <angelbreaux26@gmail.com>
2021-04-24 11:42:34 -0500

    Redo spec for all forecast data

--- spec/requests/api/v1/forecast/index_spec.rb
+++ spec/requests/api/v1/forecast/index_spec.rb
@@ -2,46 +2,27 @@ require 'rails_helper'
 RSpec.describe 'Retrieving weather for a city' do
   describe 'happy path' do
     it 'gets current_weather' do
-      VCR.use_cassette('forecast/happy_path/current_weather') do
+      VCR.use_cassette('forecast') do
         get '/api/v1/forecast?location=denver,co'
 
         expect(response).to be_successful
 
-        current_weather = JSON.parse(response.body, symbolize_names: true)
+        data = JSON.parse(response.body, symbolize_names: true)
 
-        expect(current_weather[:data][:attributes].count).to eq(10)
+        expect(data[:data][:attributes].count).to eq(3)
 
-        expect(current_weather[:data]).to have_key(:id)
-        expect(current_weather[:data]).to have_key(:type)
-        expect(current_weather[:data][:attributes]).to have_key(:datetime)
-        expect(current_weather[:data][:attributes]).to have_key(:sunrise)
-        expect(current_weather[:data][:attributes]).to have_key(:sunset)
-        expect(current_weather[:data][:attributes]).to have_key(:temperature)
-        expect(current_weather[:data][:attributes]).to have_key(:feels_like)
-        expect(current_weather[:data][:attributes]).to have_key(:humidity)
-        expect(current_weather[:data][:attributes]).to have_key(:uvi)
-        expect(current_weather[:data][:attributes]).to have_key(:visibility)
-        expect(current_weather[:data][:attributes]).to have_key(:conditions)
-        expect(current_weather[:data][:attributes]).to have_key(:icon)
+        expect(data[:data]).to have_key(:id)
+        expect(data[:data]).to have_key(:type)
+        expect(data[:data][:attributes]).to have_key(:current_weather)
+        expect(data[:data][:attributes]).to have_key(:daily_weather)
+        expect(data[:data][:attributes]).to have_key(:hourly_weather)
 
-        expect(current_weather[:data][:attributes]).not_to have_key(:pressure)
-        expect(current_weather[:data][:attributes]).not_to have_key(:dew_point)
-        expect(current_weather[:data][:attributes]).not_to have_key(:clouds)
-        expect(current_weather[:data][:attributes]).not_to have_key(:wind_speed)
-        expect(current_weather[:data][:attributes]).not_to have_key(:wind_deg)
-
-        expect(current_weather[:data][:id]).to be_nil
-        expect(current_weather[:data][:type]).to eq('current_weather')
-        expect(current_weather[:data][:attributes][:datetime]).to be_a(String)
-        expect(current_weather[:data][:attributes][:sunrise]).to be_a(String)
-        expect(current_weather[:data][:attributes][:sunset]).to be_a(String)
-        expect(current_weather[:data][:attributes][:temperature]).to be_a(Float)
-        expect(current_weather[:data][:attributes][:feels_like]).to be_a(Float)
-        expect(current_weather[:data][:attributes][:humidity]).to be_a(Float).or be_a(Integer)
-        expect(current_weather[:data][:attributes][:uvi]).to be_a(Float).or be_a(Integer)
-        expect(current_weather[:data][:attributes][:visibility]).to be_a(Float).or be_a(Integer)
-        expect(current_weather[:data][:attributes][:conditions]).to be_a(String)
-        expect(current_weather[:data][:attributes][:icon]).to be_a(String)
+        expect(data[:data][:id]).to be_nil
+        expect(data[:data][:type]).to eq('forecast')
+        
+        current_weather_test(data[:data][:attributes][:current_weather])
+        daily_weather_test(data[:data][:attributes][:daily_weather])
+        hourly_weather_test(data[:data][:attributes][:hourly_weather])
       end
     end
   end
@@ -63,6 +44,100 @@ RSpec.describe 'Retrieving weather for a city' do
       expect(response.status).to eq(406)
       data = JSON.parse(response.body, symbolize_names: true)
       expect(data[:error]).to eq('Invalid location.')
+    end
+  end
+
+  def current_weather_test(data)
+    expect(data).to have_key(:datetime)
+    expect(data).to have_key(:sunrise)
+    expect(data).to have_key(:sunset)
+    expect(data).to have_key(:temperature)
+    expect(data).to have_key(:feels_like)
+    expect(data).to have_key(:humidity)
+    expect(data).to have_key(:uvi)
+    expect(data).to have_key(:visibility)
+    expect(data).to have_key(:conditions)
+    expect(data).to have_key(:icon)
+
+    expect(data).not_to have_key(:pressure)
+    expect(data).not_to have_key(:dew_point)
+    expect(data).not_to have_key(:clouds)
+    expect(data).not_to have_key(:wind_speed)
+    expect(data).not_to have_key(:wind_deg)
+
+    expect(data[:datetime]).to be_a(String)
+    expect(data[:sunrise]).to be_a(String)
+    expect(data[:sunset]).to be_a(String)
+    expect(data[:temperature]).to be_a(Float)
+    expect(data[:feels_like]).to be_a(Float)
+    expect(data[:humidity]).to be_a(Float).or be_a(Integer)
+    expect(data[:uvi]).to be_a(Float).or be_a(Integer)
+    expect(data[:visibility]).to be_a(Float).or be_a(Integer)
+    expect(data[:conditions]).to be_a(String)
+    expect(data[:icon]).to be_a(String)
+  end
+
+  def daily_weather_test(daily_data)
+    expect(daily_data.count).to eq(5)
+
+    daily_data.each do |data|
+      expect(data).to have_key(:date)
+      expect(data).to have_key(:sunrise)
+      expect(data).to have_key(:sunset)
+      expect(data).to have_key(:max_temp)
+      expect(data).to have_key(:min_temp)
+      expect(data).to have_key(:conditions)
+      expect(data).to have_key(:icon)
+
+      expect(data).not_to have_key(:moonrise)
+      expect(data).not_to have_key(:moonset)
+      expect(data).not_to have_key(:moon_phase)
+      expect(data).not_to have_key(:feels_like)
+      expect(data).not_to have_key(:pressure)
+      expect(data).not_to have_key(:humidity)
+      expect(data).not_to have_key(:dew_point)
+      expect(data).not_to have_key(:wind_speed)
+      expect(data).not_to have_key(:wind_deg)
+      expect(data).not_to have_key(:wind_gust)
+      expect(data).not_to have_key(:clouds)
+      expect(data).not_to have_key(:pop)
+      expect(data).not_to have_key(:uvi)
+
+      expect(data[:date]).to be_a(String)
+      expect(data[:sunrise]).to be_a(String)
+      expect(data[:sunset]).to be_a(String)
+      expect(data[:max_temp]).to be_a(Float)
+      expect(data[:min_temp]).to be_a(Float)
+      expect(data[:conditions]).to be_a(String)
+      expect(data[:icon]).to be_a(String)
+    end
+  end
+
+  def hourly_weather_test(hourly_data)
+    expect(hourly_data.count).to eq(8)
+
+    hourly_data.each do |data|
+      expect(data).to have_key(:time)
+      expect(data).to have_key(:temperature)
+      expect(data).to have_key(:conditions)
+      expect(data).to have_key(:icon)
+
+      expect(data).not_to have_key(:feels_like)
+      expect(data).not_to have_key(:pressure)
+      expect(data).not_to have_key(:humidity)
+      expect(data).not_to have_key(:dew_point)
+      expect(data).not_to have_key(:uvi)
+      expect(data).not_to have_key(:clouds)
+      expect(data).not_to have_key(:visibility)
+      expect(data).not_to have_key(:wind_speed)
+      expect(data).not_to have_key(:wind_deg)
+      expect(data).not_to have_key(:wind_gust)
+      expect(data).not_to have_key(:pop)
+
+      expect(data[:time]).to be_a(String)
+      expect(data[:temperature]).to be_a(Float)
+      expect(data[:conditions]).to be_a(String)
+      expect(data[:icon]).to be_a(String)
     end
   end
 end
