df09735335626fadcde424c88cb389306182bc27
abreaux26 <angelbreaux26@gmail.com>
2021-04-24 11:48:04 -0500

    Redo facade spec

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -4,8 +4,18 @@ RSpec.describe ForecastFacade do
   describe "class methods" do
     it "::get_forecast" do
       VCR.use_cassette('facade/get_forecast') do
-        data = ForecastFacade.get_forecast('denver,co')
-        expect(data).to be_instance_of(CurrentWeather)
+        forecast = ForecastFacade.get_forecast('denver,co')
+        expect(forecast).to be_instance_of(Forecast)
+        expect(forecast.current_weather).to be_instance_of(CurrentWeather)
+        expect(forecast.daily_weather).to be_an(Array)
+        expect(forecast.hourly_weather).to be_an(Array)
+        forecast.daily_weather.each do |daily_weather|
+          expect(daily_weather).to be_instance_of(DailyWeather)
+        end
+
+        forecast.hourly_weather.each do |hourly_weather|
+          expect(hourly_weather).to be_instance_of(HourlyWeather)
+        end
       end
     end
 
