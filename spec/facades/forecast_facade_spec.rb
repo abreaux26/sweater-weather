46c345dc9d72529d293d069635752419b2be2e05
abreaux26 <angelbreaux26@gmail.com>
2021-04-24 12:28:23 -0500

    Create more specs

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -20,6 +20,63 @@ RSpec.describe ForecastFacade do
       end
     end
 
+    it "::forecast_data" do
+      VCR.use_cassette('facade/forecast_data') do
+        coords = Coordinate.new({
+            "lat": 39.738453,
+            "lng": -104.984853
+          })
+        forecast = OpenWeatherService.forecast(coords)
+        data = ForecastFacade.forecast_data(forecast)
+
+        expect(data).to be_a(Hash)
+        expect(data).to have_key(:current_weather)
+        expect(data).to have_key(:daily_weather)
+        expect(data).to have_key(:hourly_weather)
+      end
+    end
+
+    it "::current_weather" do
+      VCR.use_cassette('facade/current_weather') do
+        coords = Coordinate.new({
+            "lat": 39.738453,
+            "lng": -104.984853
+          })
+        forecast = OpenWeatherService.forecast(coords)
+        data = ForecastFacade.current_weather(forecast[:current_weather])
+
+        expect(data).to be_instance_of(CurrentWeather)
+      end
+    end
+
+    it "::daily_weather" do
+      VCR.use_cassette('facade/daily_weather') do
+        coords = Coordinate.new({
+            "lat": 39.738453,
+            "lng": -104.984853
+          })
+        forecast = OpenWeatherService.forecast(coords)
+        data = ForecastFacade.daily_weather(forecast[:daily_weather])
+
+        expect(data).to be_an(Array)
+        expect(data.size).to eq(5)
+      end
+    end
+
+    it "::hourly_weather" do
+      VCR.use_cassette('facade/hourly_weather') do
+        coords = Coordinate.new({
+            "lat": 39.738453,
+            "lng": -104.984853
+          })
+        forecast = OpenWeatherService.forecast(coords)
+        data = ForecastFacade.hourly_weather(forecast[:hourly_weather])
+
+        expect(data).to be_an(Array)
+        expect(data.size).to eq(8)
+      end
+    end
+
     it "::coordinates" do
       VCR.use_cassette('facade/coordinates') do
         data = ForecastFacade.coordinates('denver,co')
