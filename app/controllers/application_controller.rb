73aa03cd11ee42bf149bfd666c217d0577bda1d2
abreaux26 <angelbreaux26@gmail.com>
2021-04-25 16:04:22 -0500

    Fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
-
   private
 
   def invalid_location?
@@ -7,6 +6,6 @@ class ApplicationController < ActionController::API
   end
 
   def render_error(error, status = :bad_request)
-    render json: { message: "your request cannot be completed", error: error}, status: status
+    render json: { message: 'your request cannot be completed', error: error }, status: status
   end
 end
