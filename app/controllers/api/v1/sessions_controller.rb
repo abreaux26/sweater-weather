ae8ed85d5c20ccc26f4c91fb5232181b0c4b39f1
abreaux26 <angelbreaux26@gmail.com>
2021-04-25 16:33:52 -0500

    Create sessions controller and clean up users controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user_info = JSON.parse(request.raw_post, symbolize_names: :true)
+    user = User.find_by(email: user_info[:email])
+    if user && user.authenticate(user_info[:password])
+      render json: UsersSerializer.new(user)
+    else
+      render_error('Credentials are bad. Try again.', :not_found)
+    end
+  end
+end
