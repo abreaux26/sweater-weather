09958d1492c101edc1a229432d32b88a3c817593
abreaux26 <angelbreaux26@gmail.com>
2021-04-25 16:35:30 -0500

    Fix rubocop

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user_info = JSON.parse(request.raw_post, symbolize_names: :true)
+    user_info = JSON.parse(request.raw_post, symbolize_names: true)
     user = User.find_by(email: user_info[:email])
     if user && user.authenticate(user_info[:password])
       render json: UsersSerializer.new(user)
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user_info = JSON.parse(request.raw_post, symbolize_names: :true)
+    user_info = JSON.parse(request.raw_post, symbolize_names: true)
     downcase(user_info)
     user = User.create(user_info)
     return render json: UsersSerializer.new(user), status: :created if user.save
+
     render_error(user.errors.full_messages.to_sentence)
   end
 
