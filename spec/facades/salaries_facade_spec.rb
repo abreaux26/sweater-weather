d984d41a4fd78a70c81d9f87c2d3de7330eae684
abreaux26 <angelbreaux26@gmail.com>
2021-04-26 11:49:56 -0500

    Update salary facade spec correclty

--- spec/facades/salaries_facade_spec.rb
+++ spec/facades/salaries_facade_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe SalariesFacade do
   describe "class methods" do
     it "::get_salaries" do
-        salaries_info = SalariesFacade.get_salaries('chicago')
-        expect(salaries_info).to be_instance_of(Salary)
-        expect(salaries_info.destination).to eq('chicago')
-        expect(salaries_info.forecast).to be_a(Hash)
-        expect(salaries_info.salaries).to be_an(Array)
+      forecast = ForecastFacade.get_forecast('chicago')
+      salaries_info = SalariesFacade.get_salaries('chicago', forecast)
+      expect(salaries_info).to be_a(Hash)
+      expect(salaries_info[:destination]).to eq('chicago')
+      expect(salaries_info[:forecast]).to be_a(Hash)
+      expect(salaries_info[:salaries]).to be_an(Array)
     end
   end
 end
