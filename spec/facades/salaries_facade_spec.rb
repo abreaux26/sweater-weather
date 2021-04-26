6d74b70775faffd4f4c3c48881f85d949d2620f6
abreaux26 <angelbreaux26@gmail.com>
2021-04-26 12:17:51 -0500

    Update facade spec to look for salary instance

--- spec/facades/salaries_facade_spec.rb
+++ spec/facades/salaries_facade_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe SalariesFacade do
     it "::get_salaries" do
       forecast = ForecastFacade.get_forecast('chicago')
       salaries_info = SalariesFacade.get_salaries('chicago', forecast)
-      expect(salaries_info).to be_a(Hash)
-      expect(salaries_info[:destination]).to eq('chicago')
-      expect(salaries_info[:forecast]).to be_a(Hash)
-      expect(salaries_info[:salaries]).to be_an(Array)
+      expect(salaries_info).to be_instance_of(Salary)
+      expect(salaries_info.destination).to eq('chicago')
+      expect(salaries_info.forecast).to be_a(Hash)
+      expect(salaries_info.salaries).to be_an(Array)
     end
   end
 end
