ffe3f48aeb78b463d37df7aab13b7a62649e2ccc
abreaux26 <angelbreaux26@gmail.com>
2021-04-26 15:00:53 -0500

    Rename road trip serializer in controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::RoadTripController < ApplicationController
     user = User.find_by(api_key: road_trip_info[:api_key])
     road_trip_details = RoadTripFacade.get_details(road_trip_info)
     if user
-      render json: RoadTripSerializer.new(road_trip_details)
+      render json: RoadtripSerializer.new(road_trip_details)
     else
       render_error('Unauthorized. Try again.', :unauthorized)
     end
