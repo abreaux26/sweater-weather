41962e84330aa5bedde5e752375228d95b32e40e
abreaux26 <angelbreaux26@gmail.com>
2021-04-26 15:01:12 -0500

    Refactor road trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -9,7 +9,7 @@ class RoadTripFacade
       start_city: city(road_trip[:locations].first),
       end_city: city(road_trip[:locations].last),
       travel_time: road_trip[:formattedTime],
-      weather_eta_at: current_weather(road_trip[:locations].last[:latLng], road_trip[:formattedTime])
+      weather_at_eta: current_weather(road_trip[:locations].last[:latLng], road_trip[:formattedTime])
     }
   end
 
@@ -17,23 +17,33 @@ class RoadTripFacade
     "#{location[:adminArea5]}, #{location[:adminArea3]}"
   end
 
-  def self.current_weather(coords, travel_time)
+  def self.current_weather(coords, travel_duration)
     forecast = OpenWeatherService.forecast(Coordinate.new(coords))
-    current_time = Time.at(forecast[:current_weather][:dt])
-    hours, minutes = travel_time.split(':')
-    new_minutes = current_time.min + minutes.to_i
-    new_hour = if new_minutes > 60
-                  (current_time.hour + hours.to_i) + (new_minutes / 60)
-               else
-                  (current_time.hour + hours.to_i)
-                end
 
-    arrival_time = Time.new(current_time.year, current_time.month, current_time.day, new_hour, new_minutes % 60)
+    arrival_time = arrival_time(travel_duration, forecast[:current_weather][:dt])
 
     eta_weather = forecast[:hourly_weather].find do |data|
       Time.at(data[:dt]).hour == arrival_time.hour
     end
 
-    binding.pry
+    {
+      temperature: eta_weather[:temp],
+      conditions: eta_weather[:weather].first[:description]
+    }
+  end
+
+  def self.arrival_time(travel_duration, current_time)
+    current_time = Time.at(current_time)
+
+    hours, minutes = travel_duration.split(':')
+
+    new_minutes = current_time.min + minutes.to_i
+    new_hour = if new_minutes > 60
+                 (current_time.hour + hours.to_i) + (new_minutes / 60)
+               else
+                 (current_time.hour + hours.to_i)
+               end
+
+    Time.new(current_time.year, current_time.month, current_time.day, new_hour, new_minutes % 60)
   end
 end
