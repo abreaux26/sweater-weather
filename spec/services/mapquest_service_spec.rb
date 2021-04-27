7f0333e61e2475f93712b411976c9c6a1c43c497
abreaux26 <angelbreaux26@gmail.com>
2021-04-26 22:37:53 -0500

    Update mapquest service spec

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -13,6 +13,26 @@ RSpec.describe 'Mapquest Service' do
         expect(coords[:lng]).to be_a(Float)
       end
     end
+
+    it '::get_directions(trip_info)' do
+      VCR.use_cassette('mapquest_service/directions') do
+        user1 = User.create( email: "whatever@example.com",
+                              password: "password",
+                              password_confirmation: "password"
+                            )
+        road_trip_info = {
+                            "origin": "Denver,CO",
+                            "destination": "Pueblo,CO",
+                            "api_key": user1.api_key
+                          }
+
+        directions = MapquestService.get_directions(road_trip_info)
+
+        expect(directions).to be_a(Hash)
+        expect(directions).to have_key(:locations)
+        expect(directions).to have_key(:formattedTime)
+      end
+    end
   end
 
   describe 'sad path' do
